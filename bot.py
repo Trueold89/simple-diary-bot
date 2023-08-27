ea5e60f185fc00ad7a20b2860cf47c157ccddb72
trueold89 <trueold89@orudo.ru>
2023-08-27 17:55:57 +0300

    Set English as default branch language

--- bot.py
+++ bot.py
@@ -12,18 +12,18 @@ diary_bot = telebot.TeleBot(TOKEN)
 @diary_bot.message_handler(commands=['start', 'help'])
 def welcome(message):
     keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-    today = telebot.types.KeyboardButton('Сегодня')
-    next = telebot.types.KeyboardButton('Завтра')
+    today = telebot.types.KeyboardButton('Today')
+    next = telebot.types.KeyboardButton('Tomorrow')
     keyboard.add(today)
     keyboard.add(next)
     diary_bot.reply_to(message, functions.welcome(message.from_user.id, message.from_user.first_name), reply_markup=keyboard)
 
 #Send functions
-@diary_bot.message_handler(func=lambda message: message.text == 'Сегодня')
+@diary_bot.message_handler(func=lambda message: message.text == 'Today')
 def today(message):
     diary_bot.send_message(message.chat.id, functions.exec(message.from_user.id, 'today'))
 
-@diary_bot.message_handler(func=lambda message: message.text == 'Завтра')
+@diary_bot.message_handler(func=lambda message: message.text == 'Tomorrow')
 def next(message):
     diary_bot.send_message(message.chat.id, functions.exec(message.from_user.id, 'next'))
 
--- functions.py
+++ functions.py
@@ -14,7 +14,7 @@ def auth(id):
 
 def welcome(tid, user):
     if auth(tid):
-        return f'Добро пожаловать, {user}\n\nЧтобы просмотреть расписание воспользуйтесь меню ниже или отправьте номер дня\n\n(понедельник - 1, вторник -2, и т.д)'
+        return f"Welcome, {user}\n\nTo view the schedule use the menu below or send the day's number\n\n(Monday is 1, Tuesday is 2, etc.)"
     else:
         return 'Этот бот запривачен, гнида блять'
 
@@ -29,9 +29,9 @@ def currentday():
 ## Return output message
 def message(id):
     if id > 4:
-        return 'Сегодня нет занятий'
+        return "There's no schedule today"
     else:
-        days = { 0: 'Понедельник', 1: 'Вторник', 2: 'Среда', 3: 'Четверг', 4: 'Пятница' }
+        days = { 0: 'Monday', 1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday', 4: 'Friday' }
         output = str(days.get(id)) + ':\n'
         for i in diary[id]:
             output = output + i + '\n'
@@ -41,7 +41,7 @@ def message(id):
 def output(action):
     if isinstance(action,(int)):
         if action > 5:
-            return f'Дня недели под номером {action} не существует'
+            return f'Day number {action} does not exist'
         else:
             id = action - 1
             return message(id)
@@ -60,4 +60,4 @@ def exec(tid,action):
     if auth(tid):
         return output(action)
     else:
-        return 'Этот бот запривачен, гнида блять'
+        return 'Access denied'
